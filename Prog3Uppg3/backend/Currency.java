0ca7ab94f411a78cbc7712c44109b2dbb6e66382
albert <albert24lindberg@gmail.com>
2017-03-05 22:30:22 +0200

    Continued working on the backend

---
+++ Prog3Uppg3/backend/Currency.java
@@ -0,0 +1,56 @@
+package backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Currency {
+	
+	//http://ichart.finance.yahoo.com/table.csv?s=SEK=X&a=2&b=7&c=2015&d=3&e=7&f=2015&g=d&ignore=.csv
+	private String url;
+	private ArrayList<String> currValues;
+	
+	public Currency(String curr, String[] info1, String[] info2){
+		currValues = new ArrayList<>();
+		url = "http://ichart.finance.yahoo.com/table.csv?s="
+				+ curr +"=X&a="+ info1[1] + "&b=" + info1[0] +
+				"&c="+ info1[2] +"&d="+ info2[1] +"&e="+ info2[0] 
+				+"&f="+ info2[2] +"&g=d&ignore=.csv";
+		System.out.println(url);
+	}
+	
+	public Boolean read(){
+		String line = "";
+		try {
+			URL URL = new URL(url); 
+			BufferedReader buf = new BufferedReader(new InputStreamReader(URL.openStream()));
+			line = buf.readLine();
+			while (line != null) { 
+				currValues.add(line); 
+				line = buf.readLine();
+			}
+			buf.close();
+			return true;
+			
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Could not find currency file");
+			return false;
+		}
+		
+	}
+	
+	public void printCurr(){
+		for (int i = 1; i < currValues.size(); i++) {
+			System.out.println(currValues.get(i));
+		}
+	}
+	
+	public ArrayList<String> getCurr() {
+		return currValues;
+	}
+
+}
--- Prog3Uppg3/backend/Stock.java
+++ Prog3Uppg3/backend/Stock.java
@@ -12,38 +12,42 @@ public class Stock {
 	private String url;
 	private ArrayList<String> values;
 	private String stock;
-	
+	private Currency currency;
 	public Stock(){
 		values = new ArrayList<>();
 	}
 	
-	public Boolean giveURL(String stock, String date1, String date2){
+	public Boolean UppdateInfo(String stock, String date1, String date2, String curr){
+		//date comes in the following form: dd.mm.yyyy
 		//january is 00 and december is 11
 		this.stock = stock;
 		String[] info1 = date1.split("\\.");
 		String[] info2 = date2.split("\\.");
 		if (info1.length == 3 && info2.length == 3) {
+			currency = new Currency(curr, info1, info2);
+			if (!currency.read()) {
+				return false;
+			}
 			//Bygger upp urlen
 			url = "http://ichart.finance.yahoo.com/table.csv?s="
 			+ stock +"&a="+ info1[1] + "&b=" + info1[0] +
 			"&c="+ info1[2] +"&d="+ info2[1] +"&e="+ info2[0] 
 			+"&f="+ info2[2] +"&g=d&ignore=.csv";
-			System.out.println(url);
 			read();
 			return true;
 		}else{
 			return false;
 		}
-		
+		//http://ichart.finance.yahoo.com/table.csv?s=SEK=X&a=2&b=7&c=2015&d=3&e=7&f=2015&g=d&ignore=.csv
 		//http://ichart.finance.yahoo.com/table.csv?s=fb&a=02&b=19&c=2014&d=02&e=26&f=2014&g=d&ignore=.csv
 	}
 	
 	private void read(){
 		String line = "";
 		try {
-			URL URL = new URL(url); // hämtar från objektet x från dess varaibel input (som är textfield) texten, alltså urlen
+			URL URL = new URL(url); 
 			BufferedReader buf = new BufferedReader(new InputStreamReader(URL.openStream()));
-			line = buf.readLine(); // första med data
+			line = buf.readLine();
 			while (line != null) { 
 				values.add(line); 
 				line = buf.readLine();
@@ -66,17 +70,20 @@ public class Stock {
 	
 	public void print(){
 		//prints the csv file as it is
-		for (int i = 0; i < values.size(); i++) {
+		for (int i = 1; i < values.size(); i++) {
 			System.out.println(values.get(i));
 		}
+		System.out.println("\n\n\n\n");
+		currency.printCurr();
 	}
 	
 	public ArrayList<String> makeReadable(){
 		//makes a list like the one in mom1
+		ArrayList<Double> valuesNew = withCurr();
 		ArrayList<String> out = new ArrayList<>();
-		for (int i = 1; i < values.size(); i++) {
+		for (int i = 1; i < valuesNew.size(); i++) {
 			String[] temp = values.get(i).split(",");
-			out.add(temp[0] + " stock: " + stock + " close: " + temp[4]);
+			out.add(temp[0] + " stock: " + stock + " close: " + valuesNew.get(i));
 		}
 		return out;
 	}
@@ -85,8 +92,20 @@ public class Stock {
 		//The csv file has a lot of different values. type in the type field what you want
 		//0 = date, 1 = open, 2 = high, 3 = low, 4 = close, 5 = volume, 6 = adjusted close
 		ArrayList<String> temp = new ArrayList<>();
-		for (int i = 0; i < values.size(); i++) {
+		for (int i = 1; i < values.size(); i++) {
 			temp.add(values.get(i).split(",")[type]);
+		}
+		return temp;
+	}
+	
+	public ArrayList<Double> withCurr(){
+		ArrayList<String> curr = currency.getCurr();
+		ArrayList<Double> temp = new ArrayList<>();
+		for (int i = 1; i < values.size(); i++) {
+			double v = Double.parseDouble(values.get(i).split(",")[2]);
+			//System.out.println(curr.get(i).split(",")[2] + "\n");
+			double c = Double.parseDouble(curr.get(i).split(",")[2]);
+			temp.add(v*c);
 		}
 		return temp;
 	}
